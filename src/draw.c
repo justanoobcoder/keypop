96c54a11817adb07cef1ab6883fe70e888287c7b
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-04-15 15:30:47 +0700

    feat: draw F keys with the F

--- src/draw.c
+++ src/draw.c
@@ -179,22 +179,20 @@ static void draw_icon(cairo_t *cr, const char *key_name, double x, double y,
         double box_w = size * 0.45;
         double box_h = size * 0.35;
 
-        // Draw box
         cairo_set_line_width(cr, size * 0.05);
         cairo_rectangle(cr, cx - box_w / 2, cy - box_h / 2, box_w, box_h);
         cairo_stroke(cr);
 
-        // Get F number
-        const char *f_number = key_name + 1;
+        cairo_set_font_size(cr, size * 0.22);
 
-        // Draw F number
-        cairo_set_font_size(cr, size * 0.25);
         cairo_text_extents_t f_ext;
-        cairo_text_extents(cr, f_number, &f_ext);
+        cairo_text_extents(cr, key_name, &f_ext);
 
-        cairo_move_to(cr, cx - f_ext.width / 2 - f_ext.x_bearing,
-                      cy - f_ext.height / 2 - f_ext.y_bearing);
-        cairo_show_text(cr, f_number);
+        double text_x = cx - f_ext.width / 2.0 - f_ext.x_bearing;
+        double text_y = cy - f_ext.height / 2.0 - f_ext.y_bearing;
+
+        cairo_move_to(cr, text_x, text_y);
+        cairo_show_text(cr, key_name);
 
         cairo_restore(cr);
         return;
